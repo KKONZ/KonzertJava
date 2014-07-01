7440e3ec0ab0ba6b2711615c2f3e84770676e6e4
Karl Konz <HelloGitWorld@gmail.com>
2014-07-01 12:07:04 -0500

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,17 +1,6 @@
 public class BinarySearch {
   /** Main method */
-  public static void main(String[] args) {
-    int[] myList = {2, 4, 7, 10, 11, 45, 50, 59, 60, 66, 69, 70, 79};
-
-    int i = binarySearch(list, 2);
-    int j = binarySearch(list, 11);
-    int k = binarySearch(list, 12);
-
-    System.out.println(i);
-    System.out.println(j);
-    System.out.println(k);
-  }
-
+ 
   /** Binary search to find the key in the list */
   public static int binarySearch(int[] myList, int key) {
     int low = 0;
@@ -27,6 +16,6 @@ public class BinarySearch {
         low = mid + 1;
     }
 
-    return -low - 1;
-  }
+  return -low - 1;
+  }  
 }

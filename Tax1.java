373c0b5a17b69b680f7ff215bf2fe1814e91e294
Karl Konz <HelloGitWorld@gmail.com>
2014-07-07 01:48:47 -0500

    Update Tax1

--- Tax1.java
+++ Tax1.java
@@ -11,7 +11,7 @@ public class Tax1 {
 			"(0-single filer, 1-married jointly,\n" + 
 			"2-married sperately, 3-head of household)\n" +
 					"Enter filing status: ");
-		int Stat = input.nextInt();
+		int filer = input.nextInt();
 		
 		// Prompt the user to enter taxable income
 		System.out.print("How much are you claiming as earned income?: ");
@@ -19,10 +19,10 @@ public class Tax1 {
 		
 		// Compute and display the result
 		System.out.println("Your tax amount is " +
-		(int)(compuT(stat, inc) * 100) / 100.0);
+		(int)(compuT(filer, inc) * 100) / 100.0);
 	}
 	
-	public static double compuT(int stat, double inc){
+	public static double compuT(int filer, double inc){
 		double[] rates = {0.10, 0.15, 0.25, 0.28, 0.33, 0.35};
 		
 		int[][] brackets = {
@@ -35,24 +35,24 @@ public class Tax1 {
 		double tax =0; // Tax to be computed
 		
 		// Compute tax in the first bracket
-		if (inc <= brackets[stat][0])
+		if (inc <= brackets[filer][0])
 			return tax = inc * rates[0]; // Done
 		else
-			tax = brackets[status][0] * rates[0];
+			tax = brackets[filer][0] * rates[0];
 		
 		//checks for brackets 2, 3, 4, 5
 		for (int i = 1; i < brackets[0].length; i++){
-			if (income > brackets[status][i])
-				tax += (brackets[status][i] - brackets[status][i - 1]) * 
+			if (income > brackets[filer][i])
+				tax += (brackets[filer][i] - brackets[filer][i - 1]) * 
 						rates[i];
 			else{
-				tax += (income - brackets[status][i - 1]) * rates[i];
+				tax += (income - brackets[filer][i - 1]) * rates[i];
 				return tax; // Done
 				}
 			}
 			
 		
 			// Compute tax in the last (i.e, 6th) bracket
-			return tax += (income -brackets[status][4] * rates[5]);
+			return tax += (income -brackets[filer][4] * rates[5]);
 	}
 }

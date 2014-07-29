035607575f9f2866d39e492992ddaca93a4d1d20
Karl Konz <HelloGitWorld@gmail.com>
2014-07-29 02:14:41 -0500

    Create SD.java

---
+++ SD.java
@@ -0,0 +1,38 @@
+// not quite finished...
+
+
+import java.util.*;
+public class StandardDeviation{
+  private double Mean;
+  private double x;
+  private double[] n;
+    
+    public StandardDeviation(double x, double n){
+    this.Mean = Mean;
+    this.x = x;
+    this.n = n;
+    }
+// finish get/set methods
+
+    public double ComptSd{
+      double Sd = Math.sqrt()
+    }
+    
+/* sum loop(double[] n)    
+    for(int i = 0; i < n.length; i++)
+    sum += n;
+
+  mean(sum)
+    double mean = sum/n.length;
+*/
+
+/*
+  sqDev()
+*/
+  
+ public static void main(String[]args){
+  Scanner kb = new Scanner(System.in);
+  System.out.println("Enter an array: ");
+  int n = NextInt.in();
+ }
+}

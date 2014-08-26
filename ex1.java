f7ff12ae3d3b8638bceed6ebf4dae4b91c0adb47
Karl Konz <HelloGitWorld@gmail.com>
2014-08-26 11:27:07 -0500

    Update ex1.java

--- ex1.java
+++ ex1.java
@@ -3,5 +3,19 @@ import java.io.*;
 import java.net.*;
 
 public class FirstProgram{
-  
+  public static void main(String[]args){
+    System.out.println("Hello out there");
+    System.out.println("I will add two numbers for you.");
+    System.out.println("Enter two whole numbers on a line.");
+    
+    int n1, n2;
+    
+    Scanner kb = new Scanner(System.in);
+    n1 = kb.nextInt();
+    n2 = kb.nextInt();
+    
+    System.out.println("The sum of the two numbers is: ");
+    System.out.println(n1 + n2);
+    
+  }
 }

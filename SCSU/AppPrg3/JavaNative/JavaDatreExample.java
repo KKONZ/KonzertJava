107ed21ae2cb29a1d908c1ccceb6b9b41ac7225b
Karl Konz <HelloGitWorld@gmail.com>
2014-09-27 12:07:59 -0500

    Create JavaDatreExample.java

---
+++ SCSU/AppPrg3/JavaNative/JavaDatreExample.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class JavaDateExample{
+  public static void main(String[]args){
+  //Create date object with current date and time.
+  Date date = new Date();
+  System.out.println("Today is " + date + "!");
+  }
+}
